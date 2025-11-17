28fe6b6e0d81c7c3e462ad58448887c4ef2cad62
Hachama_HCMUS <truonglevuhoang2707@gmail.com>
2025-11-17 13:23:14 +0700

    clean code

--- integer_to_vietnamese.cpp
+++ integer_to_vietnamese.cpp
@@ -1,69 +1,77 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 string intToVietnamese(long num)
 {
     if (num == 0)
         return "khong";
-    string digitToText[10] = {"khong", "mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin"};
-    string units[5] = {"", "nghin", "trieu", "ti", "nghin ti"}; // can add more
-    string text = "";
+
+    const string digitToText[10] = {
+        "khong", "mot", "hai", "ba", "bon",
+        "nam", "sau", "bay", "tam", "chin"
+    };
+
+    const string units[5] = {
+        "", "nghin", "trieu", "ti", "nghin ti"
+    }; // can add more
+
+    string result = "";
     int unitCnt = 0;
+
     while (num > 0)
     {
-        string threeDigits = "";
         int block = num % 1000;
-        int temp = num;
         num /= 1000;
+
         if (block != 0)
         {
             int donvi = block % 10;
-            int chuc = (block % 100) / 10;
-            int tram = block / 100;
-            // hang tram
-            if (tram == 0)
-            {
-                if (temp >= 1000)
-                    threeDigits += "khong tram";
+            int chuc  = (block % 100) / 10;
+            int tram  = block / 100;
+
+            string part = "";
+
+            // Tram
+            if (tram == 0) {
+                if (num > 0) 
+                    part += "khong tram";
+            } else {
+                part += digitToText[tram] + string(" tram");
             }
-            else
-                threeDigits += digitToText[tram] + " tram";
 
-            // hang chuc
-            if (chuc > 1)
-                threeDigits += (" " + digitToText[chuc] + " muoi");
-            else if (chuc == 1)
-                threeDigits += " muoi";
-            else if (donvi > 0)
-            {
-                if (tram == 0)
-                {
-                    if (temp >= 1000)
-                        threeDigits += " le";
-                }
+            // Chuc
+            if (chuc > 1) {
+                part += " " + digitToText[chuc] + " muoi";
+            }
+            else if (chuc == 1) {
+                part += " muoi";
+            }
+            else if (donvi > 0) {
+                part += " le";
+            }
+
+            // Donvi
+            if (donvi > 0) {
+                if (donvi == 5 && chuc != 0) 
+                    part += " lam";
                 else
-                    threeDigits += " le";
-            } // (chuc = 0)
-
-            // don vi
-            if (donvi > 0)
-            {
-                threeDigits += (donvi == 5 && chuc != 0) ? " lam" : (" " + digitToText[donvi]);
+                    part += " " + digitToText[donvi];
             }
             if (unitCnt > 0)
-            {
-                text = units[unitCnt] + " " + text;
-            }
-            text = threeDigits + " " + text;
+                result = units[unitCnt] + " " + result;
+
+            result = part + " " + result;
         }
         unitCnt++;
     }
-    if (text.back() == ' ')
-        text.pop_back();
-    if (text.front() == ' ')
-        text.erase(0, 1);
+    
+    if (!result.empty() && result.back() == ' ')
+        result.pop_back();
+    if (!result.empty() && result.front() == ' ')
+        result.erase(0, 1);
 
-    return text;
+    return result;
 }
 
 int main()
@@ -71,5 +79,5 @@ int main()
     long num;
     cin >> num;
     cout << intToVietnamese(num);
-
+    return 0;
 }
